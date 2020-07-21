4686e0147b531a8f2e9efbab9ac84c7aa7766139
Malivuca <miguelvargasdemarque@gmail.com>
2020-07-20 22:10:19 -0300

    Merge conflicts

--- frontend/src/pages/login/components/sign-in-sign-up-card.component.tsx
+++ frontend/src/pages/login/components/sign-in-sign-up-card.component.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react'
 
-<<<<<<< HEAD
-=======
 import Paper from '@material-ui/core/Paper'
->>>>>>> dashboard-page
 import Collapse from '@material-ui/core/Collapse'
 import ListItem from '@material-ui/core/ListItem'
 import ListItemText from '@material-ui/core/ListItemText'
@@ -16,36 +13,19 @@ import { SignUp } from './sign-up.component'
 
 import styled from 'styled-components'
 
-const StyledDiv = styled.div`
-  display: flex;  
+const StyledPaper = styled.div`
   padding: 20px;
   width: min(320px, 100vw);
   border-radius: 5px;
   background-color: rgba(255, 255, 255, 0.9);
-  flex-direction: column;
-  flex-shrink: 1;
   /* margin-right: 20px; */
   /* margin-left: auto; */
-  height: auto;
   /* @media (max-width: 600px) {
     margin: auto;
   } */
   /* font-family: 'Grenze Gotisch', cursive; font-size: 40px; */
   width: min(330px, 100vw);
 
-<<<<<<< HEAD
-const SignInSignUpCard: React.FC = () => {
-  const [tab, setTab] = useState('signin')
-
-  // const [email, setEmail] = useState('')
-
-  // const [username, setUsername] = useState('')
-
-  // const [password, setPassword] = useState('')
-
-  // const [cPassword, setCpassword] = useState('')
-
-=======
   @media (max-width: 600px) {
     margin: auto;
   }
@@ -54,10 +34,9 @@ const SignInSignUpCard: React.FC = () => {
 const SignInSignUpCard: React.FC = () => {
   const [tab, setTab] = useState('signin')
 
->>>>>>> dashboard-page
   return (
     <StylesProvider injectFirst>
-      <StyledDiv>
+      <StyledPaper elevation={3}>
         <SignIn />
         <ListItem
           style={{ marginTop: '15px' }}
@@ -76,7 +55,7 @@ const SignInSignUpCard: React.FC = () => {
         <Collapse in={tab === 'signup'}>
           <SignUp />
         </Collapse>
-      </StyledDiv>
+      </StyledPaper>
     </StylesProvider>
   )
 }
