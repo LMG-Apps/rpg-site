739827c6d8e2c9e17779d95c8b29f6ab7b181d8d
Malivuca <miguelvargasdemarque@gmail.com>
2020-07-27 21:49:10 -0300

    Created optmized build of the website

---
+++ frontend/build/precache-manifest.257ee579b79c263309c686109fe6b932.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21e36777758482e130d73d7551c1a9b",
+    "url": "/rpg-site/index.html"
+  },
+  {
+    "revision": "7450b126d0b58ac40f79",
+    "url": "/rpg-site/static/css/main.2a16912c.chunk.css"
+  },
+  {
+    "revision": "c225bf33221080c1a6a1",
+    "url": "/rpg-site/static/js/2.3e3007cc.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/rpg-site/static/js/2.3e3007cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7450b126d0b58ac40f79",
+    "url": "/rpg-site/static/js/main.1830a83b.chunk.js"
+  },
+  {
+    "revision": "b046e9eece7eae72b703",
+    "url": "/rpg-site/static/js/runtime-main.7cbe2882.js"
+  },
+  {
+    "revision": "33a29fe53573c50239aa4e64eab179e7",
+    "url": "/rpg-site/static/media/Logo2.33a29fe5.png"
+  },
+  {
+    "revision": "4c25125f23d755f3bcd400dce8a287e3",
+    "url": "/rpg-site/static/media/campire1.4c25125f.jpeg"
+  },
+  {
+    "revision": "85cb1f0d862afd4508e70f31177f570d",
+    "url": "/rpg-site/static/media/campire2.85cb1f0d.jpeg"
+  }
+]);
