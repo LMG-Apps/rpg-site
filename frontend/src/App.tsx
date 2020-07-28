2fd0b9071241d21511cf400ceea345df65376d31
Malivuca <miguelvargasdemarque@gmail.com>
2020-07-27 22:27:48 -0300

    Changed BrowserRouter to HashRouter for rendering pages at gh-pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import LoginPage from './pages/login/login.page'
 import Cookies from 'universal-cookie'
 
