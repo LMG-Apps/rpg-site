f9731b4b9acc82a166e86ce230aa2a0d2bb301d7
Malivuca <Malivuca@users.noreply.github.com>
2021-03-12 06:56:30 +0000

    Deploying to gh-pages from  @ 9b3f8ea1cd5c7125c8d46d30178c652fa040d631 🚀

---
+++ precache-manifest.c51a6e367691a9addc89bb1623a23647.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2895e458af185e8e9724104ab69605d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df88906e10abfe7c9676",
+    "url": "/static/js/2.53495076.chunk.js"
+  },
+  {
+    "revision": "eefcbdf4b681a2d06664c69cdf2ad409",
+    "url": "/static/js/2.53495076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b5e5af87f516e8f893f",
+    "url": "/static/js/main.b68daa3b.chunk.js"
+  },
+  {
+    "revision": "d1cc54751cf486b1f6a5",
+    "url": "/static/js/runtime-main.91b5622f.js"
+  },
+  {
+    "revision": "33a29fe53573c50239aa4e64eab179e7",
+    "url": "/static/media/Logo2.33a29fe5.png"
+  },
+  {
+    "revision": "4c25125f23d755f3bcd400dce8a287e3",
+    "url": "/static/media/campire1.4c25125f.jpeg"
+  },
+  {
+    "revision": "85cb1f0d862afd4508e70f31177f570d",
+    "url": "/static/media/campire2.85cb1f0d.jpeg"
+  },
+  {
+    "revision": "9da4d348ccd4ad7f7ad9ae55cdad62a6",
+    "url": "/static/media/google.9da4d348.svg"
+  }
+]);
